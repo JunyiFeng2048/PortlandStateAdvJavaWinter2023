5e0036de8619a8c4727b05d13b24da241cb1e6d3
JunyiF <junyifeng98@gmail.com>
2023-01-19 12:38:11 -0800

    Added student test

--- student/src/main/java/edu/pdx/cs410J/jfeng/Student.java
+++ student/src/main/java/edu/pdx/cs410J/jfeng/Student.java
@@ -3,43 +3,71 @@ package edu.pdx.cs410J.jfeng;
 import edu.pdx.cs410J.lang.Human;
 
 import java.util.ArrayList;
-                                                                                    
+import java.util.Objects;
+
 /**                                                                                 
  * This class is represents a <code>Student</code>.                                 
- */                                                                                 
-public class Student extends Human {                                                
-                                                                                    
-  /**                                                                               
-   * Creates a new <code>Student</code>                                             
-   *                                                                                
-   * @param name                                                                    
-   *        The student's name                                                      
-   * @param classes                                                                 
-   *        The names of the classes the student is taking.  A student              
-   *        may take zero or more classes.                                          
-   * @param gpa                                                                     
-   *        The student's grade point average                                       
-   * @param gender                                                                  
+ */
+
+
+
+public class Student extends Human {
+
+  /**
+   * Creates a new <code>Student</code>
+   *
+   * @param name
+   *        The student's name
+   * @param classes
+   *        The names of the classes the student is taking.  A student
+   *        may take zero or more classes.
+   * @param gpa
+   *        The student's grade point average
+   * @param gender
    *        The student's gender ("male", "female", or "other", case insensitive)
-   */                                                                               
+   */
+
+  private final ArrayList<String> classes;
   public Student(String name, ArrayList<String> classes, double gpa, String gender) {
     super(name);
+
+    validateNotNull(name);
+    validateNotNull(classes);
+    validateNotNull(gender);
+
+    this.classes = classes;
   }
 
-  /**                                                                               
+  private static void validateNotNull(Object obj) {
+    if(obj == null)
+    {
+      throw new NullPointerException();
+    }
+  }
+
+  /**
    * All students say "This class is too much work"
    */
   @Override
-  public String says() {                                                            
+  public String says() {
     return "This class is to much work!";
   }
-                                                                                    
-  /**                                                                               
-   * Returns a <code>String</code> that describes this                              
-   * <code>Student</code>.                                                          
-   */                                                                               
+
+  /**
+   * Returns a <code>String</code> that describes this
+   * <code>Student</code>.
+   */
   public String toString() {
-    throw new UnsupportedOperationException("Not implemented yet");
+    //throw new UnsupportedOperationException("Not implemented yet");
+
+    int NumberOfClasses = getNumberOfClasses();
+
+    return "is taking " + NumberOfClasses + " classes";
+  }
+
+  private static int getNumberOfClasses()
+  {
+    return 1;
   }
 
   /**
--- student/src/test/java/edu/pdx/cs410J/jfeng/StudentTest.java
+++ student/src/test/java/edu/pdx/cs410J/jfeng/StudentTest.java
@@ -5,15 +5,16 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Unit tests for the Student class.  In addition to the JUnit annotations,
  * they also make use of the <a href="http://hamcrest.org/JavaHamcrest/">hamcrest</a>
  * matchers for more readable assertion statements.
  */
-public class StudentTest
-{
+public class StudentTest {
 
   @Test
   void studentNamedPatIsNamedPat() {
@@ -25,7 +26,55 @@ public class StudentTest
   @Test
   void ThisClassIsToMuchWork() {
     Student student = new Student("Name", new ArrayList<>(), 0.0, "NO!!!");
-    assertThat(student.says(),equalTo("This class is to much work!"));
+    assertThat(student.says(), equalTo("This class is to much work!"));
   }
+
+  @Test
+  void StudentSaysWhatIsExpected()
+  {
+    ArrayList<String> classes = new ArrayList<>();
+
+    classes.add("AI");
+    classes.add("Web Dev");
+    classes.add("Operating Systems");
+
+    Student john = new Student("John", classes, 4.0, "male");
+
+    //String johnString = john.toString();
+    //assertThat(johnString, equalTo(johnString));
+  }
+
+  @Test
+  void NullNameThrowsNullPointerException()
+  {
+    assertThrows(NullPointerException.class, () -> new Student(null, null, 4.0, null));
+  }
+
+  @Test
+  void NullClassesThrowsNullPointerException()
+  {
+    assertThrows(NullPointerException.class, () -> new Student("Name", null, 4.0, null));
+  }
+
+  @Test
+  void NullGenderThrowsNullPointerException()
+  {
+    assertThrows(NullPointerException.class, () -> new Student("Name", new ArrayList<>(), 4.0, null));
+  }
+
+  @Test
+  void studentCanTakeOneClass()
+  {
+    ArrayList<String> classes = new ArrayList<>();
+
+    classes.add("AI");
+    classes.add("Web Dev");
+    classes.add("Operating Systems");
+
+    Student john = new Student("John", classes, 4.0, "male");
+    String johnString = john.toString();
+    assertThat(johnString, containsString("is taking 1 class"));
+  }
+
 
 }
